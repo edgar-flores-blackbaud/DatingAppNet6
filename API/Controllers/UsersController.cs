bbbb1a61e7146661f42ab7c0771ebaf2e1f8f8ac
Edgar Flores <Edgar.Flores@blackbaud.me>
2022-07-05 17:02:59 -0500

    End of section 2

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using API.Data;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : Controller
+    {
+        private readonly ILogger<UsersController> _logger;
+        private readonly DataContext _context;
+
+        public UsersController(ILogger<UsersController> logger, DataContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(){
+            // if(_context.Users is not null)
+            //     return await _context.Users.ToListAsync();
+            // return new List<AppUser>();
+            return _context.Users is not null ? 
+                await _context.Users.ToListAsync(): 
+                new List<AppUser>();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id){
+            // if(_context.Users is not null){
+            //     var user = _context.Users.Find(id);
+            //     if(user is not null)
+            //         return user;
+            // }                
+            // return new AppUser();
+            return _context.Users is not null ? 
+                await _context.Users.FindAsync(id) : 
+                new AppUser();
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
